55ff04b0320e02278181f62dc8bb96a2e4d88eb1
Sulaymahn Mukhtar <sulmuk28@gmail.com>
2024-05-02 10:11:57 +0100

    country model

---
+++ src/models/country.ts
@@ -0,0 +1,15 @@
+export class Country{
+    name:string;
+    cities:string[];
+    constructor(name:string,cities:string[]){
+        this.name= name;
+        this.cities= cities;
+    }
+}
+
+export const countries:Country[] = [
+    new Country("", [
+        
+    ]),
+
+]
